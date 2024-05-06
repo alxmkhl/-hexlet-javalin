ec13b3b2063a6780d1df4108a18d5d79f3464f46
alxmkh <mail@com>
2024-05-06 10:07:10 +0300

    add hello handler

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -8,8 +8,13 @@ public class HelloWorld {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            ctx.result("Hello " + name + "!");
+
+        });
+        app.start(7070);
     }
 }
